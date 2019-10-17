aa9d2395d39ed83a0c27ec676c541508ff619097
Peter Neave <peter.neave@jims.net>
2019-10-17 16:27:15 +1100

    General useability improvementsUse b instead of sbAllow protoco in applicationAdded Bamboo images

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # bamboo-quick-search
-A Google Chrome omnibar extension that allows quick search of builds from Atlasian Bamboo
+
+A Google Chrome omnibar extension that allows quick search of builds from Atlassian Bamboo
 
 ## Usage
-The extension registers the 'sb' (Search Bamboo) keyword.
+
+The extension registers the 'b' (Search Bamboo) keyword.
 While typing in the search query, you will be suggested with relevant branch names.
 Scroll down the list until you have selected the desired branch, and press ENTER.
 
 ## Configuration
+
 Prior to using this extension, please make sure to configure the host address of your build server.
 
-NOTE: This extension assumes you have previously logged into Atlasian Bamboo, and will use your existing credentials/cookies.
+NOTE: This extension assumes you have previously logged into Atlassian Bamboo, and will use your existing credentials/cookies.
--- src/background.js
+++ src/background.js
@@ -1,24 +1,24 @@
 var serverAddress = "";
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-        console.info(changes);
-        for (key in changes) {
-          if (key == "serverAddress") {
-            var storageChange = changes[key];
-            serverAddress = storageChange.newValue;
-          }
-      }
-    });
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  console.info(changes);
+  for (key in changes) {
+    if (key == "serverAddress") {
+      var storageChange = changes[key];
+      serverAddress = storageChange.newValue;
+    }
+  }
+});
 
 chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    if(!text) return;
+  function (text, suggest) {
+    if (!text) return;
     var xhr = new XMLHttpRequest();
-    chrome.storage.sync.get('buildServerAddress', function(data) {
+    chrome.storage.sync.get('buildServerAddress', function (data) {
       serverAddress = data.buildServerAddress;
     });
     xhr.open("GET", "https://" + serverAddress + "/rest/api/latest/quicksearch?searchTerm=" + text, true);
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         var resp = JSON.parse(xhr.responseText);
@@ -28,7 +28,7 @@ chrome.omnibox.onInputChanged.addListener(
           suggestion = result['entity']['branchName'] + ' (' + result['entity']['key'] + ')';
           suggestionList.push({
             content: suggestion,
-            description: suggestion, 
+            description: suggestion,
           });
         }
         suggest(suggestionList);
@@ -39,14 +39,14 @@ chrome.omnibox.onInputChanged.addListener(
 
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
-  function(text) {
+  function (text) {
     // Get the key out of the suggestion.
     key = text.split(")")[0].split("(")[1]
-    navigate("https://" + serverAddress + "/browse/" + key);
+    navigate(serverAddress + "/browse/" + key);
   });
 
 function navigate(url) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url});
-    });
-  }
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.update(tabs[0].id, { url: url });
+  });
+}
